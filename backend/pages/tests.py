047a8af3b4899a76ac2cf9e3858a2ef4aa7cc3e0
elviktor <marco.flagg@gmail.com>
2021-11-01 15:26:14 -0400

    feat: Update tests.

--- backend/pages/tests.py
+++ backend/pages/tests.py
@@ -2,10 +2,22 @@ from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 
 class HomepageTests(SimpleTestCase):
+    def setUp(self):
+        url = reverse('home')
+        self.response = self.client.get(url)
+
     def test_homepage_status_code(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
-    def test_homepage_url_name(self):
-        response = self.client.get(reverse('home'))
-        self.assertEqual(response.status_code, 200)
+    def test_homepage_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_homepage_contains_correct_html(self):
+        response = self.client.get('/')
+        self.assertContains(response, 'Homepage')
+
+    def test_homepage_does_not_contain_incorrect_html(self):
+        response = self.client.get('/')
+        self.assertNotContains(response, 'Hi there! I love chicken soup.')
