1f031faeb8a3938a6ba6998d5aab690dd9060f7e
elviktor <marco.flagg@gmail.com>
2021-11-28 17:14:46 -0500

    feat: Add intake user profile form

--- backend/intakemanager/forms.py
+++ backend/intakemanager/forms.py
@@ -1,12 +1,31 @@
-from django.forms import ModelForm, CheckboxSelectMultiple, Select
+from django.forms import ModelForm, CheckboxSelectMultiple, Select, HiddenInput
 from .models import IntakeUser
+
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Fieldset
 
 class IntakeUserForm(ModelForm):
     class Meta:
         model = IntakeUser
-        fields = '__all__'
+        fields = {'user', 'first_name', 'last_name', 'area', 'msc_group'}
         widgets = {
-            'status': Select,
+            'user': HiddenInput,
             'area': CheckboxSelectMultiple,
             'msc_group': CheckboxSelectMultiple,
         }
+
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop("request")
+        super(IntakeUserForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Fieldset(
+
+                'msc_group',
+                'first_name',
+                'last_name',
+                'area'
+
+            )
+        )
+        self.fields['user'].initial = self.request.user
--- backend/intakemanager/views.py
+++ backend/intakemanager/views.py
@@ -3,14 +3,14 @@ from .models import IntakeUser
 from .forms import IntakeUserForm
 
 def intakeUserForm(request):
-    form = IntakeUserForm(request.POST)
+    form = IntakeUserForm(data=request.POST, request=request)
     if form.is_valid():
         u = form.save()
         users = IntakeUser.objects.all()
         return render(request, 'home.html')
 
     else:
-        form_class = IntakeUserForm
+        form_class = IntakeUserForm(request=request)
 
     return render(request, 'intakemanager/intakeuser_form.html', {
         'form': form_class,
