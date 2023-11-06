34d14efa0af163567bcd854ec7d5ed248dc394b0
elviktor <marco.flagg@gmail.com>
2023-11-06 13:18:08 -0500

    change

--- backend/entrypoint.prod.sh
+++ backend/entrypoint.prod.sh
@@ -15,5 +15,8 @@ python manage.py flush --no-input
 python manage.py migrate
 python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL --password $DJANGO_SUPERUSER_PASSWORD
 
+# You can put other setup logic here 
+echo "Superuser created!"
+
 # Evaluating passed command:
 exec "$@"
