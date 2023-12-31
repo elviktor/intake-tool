77008588240a334caccc1a71f2a4e95bb92e3cf6
elviktor <marco.flagg@gmail.com>
2023-12-31 11:00:21 -0500

    Added Plant model

--- backend/tracker/models.py
+++ backend/tracker/models.py
@@ -13,3 +13,27 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+class Plant(models.Model):
+    birth_date = models.DateField()
+    converted = models.BooleanField()
+    deleted = models.BooleanField()
+    destroy_reason = models.CharField(max_length=250)
+    destroy_reason_id = models.IntegerField()
+    destroy_scheduled = models.BooleanField()
+    destroy_scheduled_time = models.DateTimeField()
+    external_id = models.CharField(max_length=250)
+    harvest_scheduled = models.BooleanField()
+    id = models.CharField(max_length=250)
+    location = models.CharField(max_length=250)
+    mother = models.BooleanField()
+    org_id = models.IntegerField()
+    parent_id = models.CharField(max_length=250)
+    room_id = models.IntegerField()
+    session_time = models.IntegerField()
+    state = models.CharField(max_length=250)
+    strain = models.CharField(max_length=250)
+    transaction_id = models.IntegerField()
+
+    def __str__(self):
+        return self.id
