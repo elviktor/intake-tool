01892295161ec426a27b7e04fa9a7e3132682f55
elviktor <marco.flagg@gmail.com>
2023-12-31 12:06:50 -0500

    add makemigrations command to entrypoint.prod.sh

--- backend/entrypoint.prod.sh
+++ backend/entrypoint.prod.sh
@@ -12,6 +12,7 @@ echo "Let's go!"
 
 # Experiment...
 python manage.py flush --no-input
+python manage.py makemigrations
 python manage.py migrate
 python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
 
