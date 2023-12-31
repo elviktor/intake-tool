016b1b5dd45c7cc73853b4585ac2ffe981860e55
elviktor <marco.flagg@gmail.com>
2023-12-31 12:27:43 -0500

    Fix Plant model to make fields optional

--- backend/tracker/models.py
+++ backend/tracker/models.py
@@ -15,29 +15,29 @@ class Book(models.Model):
         return self.title
 
 class Plant(models.Model):
-    birth_date = models.DateField()
+    birth_date = models.DateField(null=True, blank=True)
     converted = models.BooleanField()
     deleted = models.BooleanField()
-    destroy_reason = models.CharField(max_length=250)
-    destroy_reason_id = models.IntegerField()
+    destroy_reason = models.CharField(max_length=250, null=True, blank=True)
+    destroy_reason_id = models.IntegerField(null=True, blank=True)
     destroy_scheduled = models.BooleanField()
-    destroy_scheduled_time = models.DateTimeField()
-    external_id = models.CharField(max_length=250)
+    destroy_scheduled_time = models.DateTimeField(null=True, blank=True)
+    external_id = models.CharField(max_length=250, null=True, blank=True)
     harvest_scheduled = models.BooleanField()
-    biotrack_id = models.CharField(max_length=250)
-    location = models.CharField(max_length=250)
+    biotrack_id = models.CharField(max_length=250, null=True, blank=True)
+    location = models.CharField(max_length=250, null=True, blank=True)
     mother = models.BooleanField()
-    org_id = models.IntegerField()
-    parent_id = models.CharField(max_length=250)
-    room_id = models.IntegerField()
-    session_time = models.IntegerField()
-    state = models.CharField(max_length=250)
-    strain = models.CharField(max_length=250)
-    transaction_id = models.IntegerField()
+    org_id = models.IntegerField(null=True, blank=True)
+    parent_id = models.CharField(max_length=250, null=True, blank=True)
+    room_id = models.IntegerField(null=True, blank=True)
+    session_time = models.IntegerField(null=True, blank=True)
+    state = models.CharField(max_length=250, null=True, blank=True)
+    strain = models.CharField(max_length=250, null=True, blank=True)
+    transaction_id = models.IntegerField(null=True, blank=True)
 
     def get_absolute_url(self):
         """Returns the url to access a particular author instance."""
-        return reverse('plant_detail', args=[str(self.id)])
+        return reverse('plant_detail', args=[str(self.biotrack_id)])
     
     def __str__(self):
-        return self.id
+        return self.biotrack_id
