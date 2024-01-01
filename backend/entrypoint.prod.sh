9e7adddf128e0d98c8a2d86531e44f7caae68db6
elviktor <marco.flagg@gmail.com>
2023-12-31 19:31:51 -0500

    update tracker app migration

--- backend/entrypoint.prod.sh
+++ backend/entrypoint.prod.sh
@@ -11,7 +11,8 @@ set -e
 echo "Let's go!"
 
 # Experiment...
-python manage.py reset_db --no-input
+python manage.py flush --no-input
+python manage.py migrate tracker zero
 python manage.py makemigrations
 python manage.py migrate
 python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
