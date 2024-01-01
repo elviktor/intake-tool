1601d41c37d846b0c12af0522d199b354489d4ef
elviktor <marco.flagg@gmail.com>
2023-12-31 20:18:37 -0500

    fix add unique id to plant

--- backend/tracker/models.py
+++ backend/tracker/models.py
@@ -15,8 +15,26 @@ class Book(models.Model):
         return self.title
 
 class Plant(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    birth_date = models.DateField(null=True, blank=True)
+    converted = models.BooleanField()
+    deleted = models.BooleanField()
+    destroy_reason = models.CharField(max_length=250, null=True, blank=True)
+    destroy_reason_id = models.IntegerField(null=True, blank=True)
+    destroy_scheduled = models.BooleanField()
+    destroy_scheduled_time = models.DateTimeField(null=True, blank=True)
+    external_id = models.CharField(max_length=250, null=True, blank=True)
+    harvest_scheduled = models.BooleanField()
     biotrack_id = models.CharField(max_length=250, null=True, blank=True)
-   
+    location = models.CharField(max_length=250, null=True, blank=True)
+    mother = models.BooleanField()
+    org_id = models.IntegerField(null=True, blank=True)
+    parent_id = models.CharField(max_length=250, null=True, blank=True)
+    room_id = models.IntegerField(null=True, blank=True)
+    session_time = models.IntegerField(null=True, blank=True)
+    state = models.CharField(max_length=250, null=True, blank=True)
+    strain = models.CharField(max_length=250, null=True, blank=True)
+    transaction_id = models.IntegerField(null=True, blank=True)
 
     def get_absolute_url(self):
         """Returns the url to access a particular author instance."""
