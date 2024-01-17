635b18d4d066a88c01ad99e9fcf992b1d8e7dd03
elviktor <marco.flagg@gmail.com>
2024-01-16 22:20:11 -0500

    Add collectstatic to entrypoint

--- backend/entrypoint.prod.sh
+++ backend/entrypoint.prod.sh
@@ -11,8 +11,9 @@ set -e
 echo "Let's go!"
 
 # Experiment...
-python manage.py flush --no-input
+#python manage.py flush --no-input
 #python manage.py migrate tracker zero
+python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
 python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
