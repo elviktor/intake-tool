e851497806ae8c11e3ad6e627747e8277b0c1ec4
elviktor <marco.flagg@gmail.com>
2024-01-16 21:55:05 -0500

    add bootstrap template libraries

--- backend/pages/views.py
+++ backend/pages/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import never_cache
 from django.views.generic import TemplateView
 from django.shortcuts import get_object_or_404
 
 from intakemanager.models import IntakeUser
 
+
 class HomePageView(TemplateView):
     template_name = 'home.html'
+    @method_decorator(never_cache)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
 
 class AboutPageView(TemplateView):
     template_name = 'about.html'
-
+    @method_decorator(never_cache)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+    
 class UserHomeView(TemplateView):
     template_name = 'intakemanager/userhome.html'
     def get_context_data(self,*args, **kwargs):
